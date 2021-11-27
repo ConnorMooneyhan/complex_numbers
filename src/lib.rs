095864abcce9d02767d4478e753781f9f4d09155
Connor Mooneyhan <connmoo@gmail.com>
2021-11-26 21:34:55 -0500

    Start Mul impl for C64 and add decimal_places helper function

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-use std::{ fmt, ops };
+use std::{ fmt, ops, cmp::Ordering };
 
 #[derive(PartialEq, Clone, Copy, Debug)]
 /// Complex Number
@@ -45,6 +45,28 @@ impl ops::Sub for C64 {
             im: self.im - other.im
         }
     }
+}
+
+impl ops::Mul for C64 {
+    type Output = Self;
+
+    fn mul(self, other: Self) -> Self::Output {
+
+                
+        Self {
+            re: self.re * other.re - self.im * other.im,
+            im: self.re * other.im + self.im * other.re
+        }
+    }
+}
+
+/// Takes an f64 as input and returns its number of decimal places
+fn decimal_places(num: f64) -> usize {
+    if num.fract() == 0.0 { return 1 }
+    
+    let num_str = num.to_string();
+    let split_str: Vec<&str> = num_str.split('.').collect();
+    split_str[1].len()
 }
 
 #[cfg(test)]
@@ -105,5 +127,43 @@ mod tests {
             C64::new(-546.3, -4002.57) - C64::new(5.546, 8.21), 
             C64::new(-551.846, -4010.78)
         );
+    }
+
+    #[test]
+    /// Tests proper implementation of Sub for C64
+    fn multiplies() {        
+        assert_eq!(
+            C64::new(1, 2) * C64::new(5, 8), 
+            C64::new(-11, 18)
+        );
+
+        assert_eq!(
+            C64::new(5, -22) * C64::new(5, 8), 
+            C64::new(201, -70)
+        );
+        assert_eq!(
+            C64::new(-180, -42) * C64::new(-51, -82), 
+            C64::new(5736, 16902)
+        );
+
+        assert_eq!(
+            C64::new(1.3, 2) * C64::new(-5, 8.7), 
+            C64::new(-23.9, 1.31)
+        );
+
+        assert_eq!(
+            C64::new(-546.3, -4002.57) * C64::new(5.546, 8.21), 
+            C64::new(35890.8795, -17713.13022)
+        );
+    }
+
+    #[test]
+    fn calculates_decimal_places() {
+        assert_eq!(decimal_places(2.93), 2);
+        assert_eq!(decimal_places(2.123456789), 9);
+        assert_eq!(decimal_places(28489.502), 3);
+        assert_eq!(decimal_places(0.93), 2);
+        assert_eq!(decimal_places(2.0), 1);
+        assert_eq!(decimal_places(0.9), 1);
     }
 }
